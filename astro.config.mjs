a4157b0f584dc9aaeb5c9778f843275a0db9678d
Jane Miceli <jane@miceli.me>
2026-02-23 15:33:55 -0700

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://akash-verma.github.io',
+  site: 'https://janemiceli.github.io',
   base: '/portfolio',
   vite: {
     plugins: [tailwindcss()]
